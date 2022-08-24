106289f8e93cdbad2f5fa33181e911d2ba39f761
Parvez785 <abdulshaikh2601@gmail.com>
2022-08-24 18:49:01 +0530

    commiting C code

---
+++ parvez.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int j;
+	printf("enter your number\n");
+	scanf("%d",&b);
+	b=b/2
+    a=a% 2;
+	    
+	}
+	return 0;
+	
+	
+	
+	
+}
+
